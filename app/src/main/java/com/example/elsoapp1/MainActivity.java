92eec0620ea947855dd252d1cf43f5a57163556a
daniel.zsombor <fcsoki@yahoo.com>
2019-10-16 13:19:50 +0300

    Lab 2

--- app/src/main/java/com/example/elsoapp1/MainActivity.java
+++ app/src/main/java/com/example/elsoapp1/MainActivity.java
@@ -1,33 +1,37 @@
-
-package com.example.elsoapp1;
+package com.example.a;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.content.Intent;
+
 import android.os.Bundle;
 import android.view.View;
+import android.content.Intent;
 import android.widget.EditText;
 
-
-
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
-
-    public static final String EXTRA_MESSAGE = "blabla" ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void goProfile(View view)
+    {
+        Intent profintent=new Intent(this,Profile.class);
+        EditText firstName=findViewById(R.id.firstName);
+        String first_name=firstName.getText().toString();
+        profintent.putExtra("FIRST_NAME",first_name);
+        EditText lastName=findViewById(R.id.lastName);
+        String last_name=lastName.getText().toString();
+        profintent.putExtra("LAST_NAME",last_name);
+        startActivityForResult(profintent,1);
 
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.elso);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+
     }
 
+    public void goRegister(View view)
+    {
+        Intent registerintent=new Intent(this,Register.class);
+        startActivity(registerintent);
+    }
 
 }
